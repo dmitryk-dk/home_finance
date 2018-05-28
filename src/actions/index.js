1dcd216d990e3c64835a4c13463765a90d629e5b
dmitryk-dk <kozlovdmitriyy@gmail.com>
2018-05-28 15:19:11 +0300

    update actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,45 +1,35 @@
 import * as actionTypes from './actionTypes';
 
 export function addBudgetItem(record) {
-  return (dispatch) => {
-    dispatch({
-      type: actionTypes.ADD_BUDGET_ITEM,
-      record,
-    });
-  } 
+  return {
+    type: actionTypes.ADD_BUDGET_ITEM,
+    record,
+  }
 }
 
 export function deleteBudgetItem(record) {
-  return (dispatch) => {
-    dispatch({
-      type: actionTypes.DELETE_BUDGET_ITEM,
-      record,
-    });
-  } 
+  return {
+    type: actionTypes.DELETE_BUDGET_ITEM,
+    record,
+  }
 }
 
 export function changeCurrencyType(currencyType) {
-  return (dispatch) => {
-    dispatch({
-      type: actionTypes.CHANGE_CURRENCY_TYPE,
-      currencyType,
-    });
+  return {
+    type: actionTypes.CHANGE_CURRENCY_TYPE,
+    currencyType,
   }
 }
 
 export function changeCurrencyRate(currencyRate) {
-  return (dispatch) => {
-    dispatch({
-      type: actionTypes.CHANGE_CURRENCY_RATE,
-      currencyRate,
-    });
+  return {
+    type: actionTypes.CHANGE_CURRENCY_RATE,
+    currencyRate,
   }
 }
 
 export function disableFilter() {
-  return (dispatch) => {
-    dispatch({
-      type: actionTypes.DISABLE_FILTER,
-    });
+  return {
+    type: actionTypes.DISABLE_FILTER,
   }
 }
--- src/components/ItemsList/index.js
+++ src/components/ItemsList/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Item from '../Item';
 export default function ItemsList({records, currencyRate, deleteBudgetItem}) {
-  console.log("Records ->>", records);
   return (
     records.map(item => (
         <Item 
